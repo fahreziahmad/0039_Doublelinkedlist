25c7ec7766617114d7ea153e755620e0f3d96b2f
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-05-04 20:47:04 +0700

    add if untuk membuat node baruu

--- Doublelinkedlist/Doublelinkedlist.cpp
+++ Doublelinkedlist/Doublelinkedlist.cpp
@@ -23,6 +23,15 @@ void addNode()
 			cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
 			return;
 		}
+		newNode->next = START;
+		if (START != NULL)
+		{
+			START->prev = newNode;
+		}
+		newNode->prev = NULL;
+		START = newNode;
+
+	}
 
 
 
